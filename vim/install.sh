37451485ee1cbc748e417873df6b6fdc9e83bf2c
odaven <18171281+odaven@users.noreply.github.com>
2022-05-20 23:58:02 +0100

    Pointing directly at the .dotfiles folder and making ln verbose

--- vim/install.sh
+++ vim/install.sh
@@ -1,8 +1,8 @@
 DOTFILES="$HOME/.dotfiles"
 
-echo "[.dotfiles/vim] Creating soft link $SCRIPTPATH/.vimrc into home directory"
-ln -sfv $DOTFILES/vim/.vimrc ~/.vimrc
-ln -sfv $DOTFILES/vim/.ideavimrc ~/.ideavimrc
+echo "[.dotfiles/vim] Creating soft link $DOTFILES/vim/.vimrc into home directory"
+ln -sfv "$DOTFILES"/vim/.vimrc ~/.vimrc
+ln -sfv "$DOTFILES"/vim/.ideavimrc ~/.ideavimrc
 
 echo "[.dotfiles/vim] Downloading vim-plug"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
--- zsh/install.sh
+++ zsh/install.sh
@@ -1,4 +1,4 @@
-SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+DOTFILES="$HOME/.dotfiles"
 
 echo "[.dotfiles/zsh] Changing zsh to default"
 sudo chsh -s $(which zsh) $(whoami)
@@ -6,14 +6,14 @@ sudo chsh -s $(which zsh) $(whoami)
 echo "[.dotfiles/zsh] Installing oh-my-zsh"
 curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash
 
-echo "[.dotfiles/zsh] Creating soft link $SCRIPTPATH/.zsh_aliases into home directory"
-ln -sf $SCRIPTPATH/.zsh_aliases ~/.zsh_aliases
+echo "[.dotfiles/zsh] Creating soft link $DOTFILES/zsh/.zsh_aliases into home directory"
+ln -sfv "$DOTFILES"/zsh/.zsh_aliases ~/.zsh_aliases
 
-echo "[.dotfiles/zsh] Creating soft link $SCRIPTPATH/.zshrc into home directory"
-ln -sf $SCRIPTPATH/.zshrc ~/.zshrc
+echo "[.dotfiles/zsh] Creating soft link $DOTFILES/zsh/.zshrc into home directory"
+ln -sfv "$DOTFILES"/zsh/.zshrc ~/.zshrc
 
 echo "[.dotfiles/zsh] Move oh-my-zsh themes to custom folder"
-cp $SCRIPTPATH/oh-my-zsh/j.zsh-theme ~/.oh-my-zsh/custom/themes/
-cp $SCRIPTPATH/oh-my-zsh/j-all.zsh-theme ~/.oh-my-zsh/custom/themes/
+cp -v "$DOTFILES"/zsh/oh-my-zsh/j.zsh-theme ~/.oh-my-zsh/custom/themes/
+cp -v "$DOTFILES"/zsh/oh-my-zsh/j-all.zsh-theme ~/.oh-my-zsh/custom/themes/
 
 echo "[.dotfiles/zsh] Restart to get the changes"
