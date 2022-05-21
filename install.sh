a31bb8a9eb3c45d655dd0f5348889706b41c0b13
odaven <18171281+odaven@users.noreply.github.com>
2022-05-21 13:30:51 +0100

    With a menu

--- install.sh
+++ install.sh
@@ -1,5 +1,14 @@
-echo "[.dotfiles] Start zsh configuration"
-source zsh/install.sh
-
-echo "[.dotfiles] Start vim configuration"
-source vim/install.sh
+echo -ne "
+[.dotfiles] System?
+1) MacOs
+2) Raspberry-pi
+0) Exit
+Choose an option: "
+read a
+case $a in
+  1) source macos/install.sh ;;
+  2) source raspberry-pi/install.sh ;;
+  0) echo "Bye" ;;
+  *) echo "Wrong option." ;;
+esac
+echo ""
