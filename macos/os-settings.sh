3bdfc75f129d2fe83185437f390c4b62ff65c6a2
odaven <18171281+odaven@users.noreply.github.com>
2022-05-30 17:33:02 +0100

    Configure key repeat elements

---
+++ macos/os-settings.sh
@@ -0,0 +1,10 @@
+###################################################
+# Keyboard
+###################################################
+
+# Disable press and hold
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
+# Fast keyrepeat and short delay until key repeat
+defaults write NSGlobalDomain KeyRepeat -int 2
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
