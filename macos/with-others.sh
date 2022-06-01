5f80fddc71b593b0f7690bc62c9808c716acca6b
odaven <18171281+odaven@users.noreply.github.com>
2022-06-01 18:12:38 +0100

    Installing python with pyenv

--- macos/with-others.sh
+++ macos/with-others.sh
@@ -4,5 +4,6 @@ echo "[.dotfiles/macos/with-others] Installing node"
 nvm install --lts
 
 echo "[.dotfiles/macos/with-others] Installing python"
-pyenv install 3.10.4
-pyenv global 3.10.4
+python_version="3.10.4"
+pyenv install $python_version
+pyenv global $python_version
