5dfef76dce0f95f64b7c78f291db41294adfdff7
odaven <18171281+odaven@users.noreply.github.com>
2022-07-01 23:37:31 +0100

    Neovim configuration with lua

--- neovim/install.sh
+++ neovim/install.sh
@@ -1,9 +1,9 @@
 echo "[.dotfiles/neovim] Creating soft links"
 ln -sfv ~/.dotfiles/neovim/nvim ~/.config
 
-echo "[.dotfiles/neovim] Install Vim-Plug"
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+echo "[.dotfiles/neovim] Install packer.nvim"
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 echo "[.dotfiles/neovim] Installing plugins"
-nvim +PlugInstall +qall
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
---
+++ neovim/nvim/lua/user/options.lua
@@ -0,0 +1,29 @@
+local options = {
+
+    cursorline = true,
+    errorbells = false,
+    expandtab = true,
+    hlsearch = true,
+    ignorecase = true,
+    incsearch = true,
+    mouse = "a",
+    number = true,
+    relativenumber = true,
+    ruler = true,
+    scrolloff = 10,
+    shiftwidth = 4,
+    signcolumn = "yes",
+    smartcase = true, 
+    smartindent = true,
+    softtabstop = 4,
+    splitbelow = true,
+    splitright = true,
+    swapfile = false,
+    tabstop = 4,
+    wrap = true
+
+}
+
+for key, value in pairs(options) do
+    vim.opt[key] = value
+end
