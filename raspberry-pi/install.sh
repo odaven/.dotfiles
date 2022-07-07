3f94d92dea3ee00e22d4df4da0e08aa8f6706794
odaven <18171281+odaven@users.noreply.github.com>
2022-07-07 14:32:47 +0100

    neovim as snap

--- raspberry-pi/install.sh
+++ raspberry-pi/install.sh
@@ -9,8 +9,12 @@ sudo apt --assume-yes upgrade
 echo "[.dotfiles/raspberry-pi] Removing unwanted packages"
 sudo apt --assume-yes remove vim-tiny
 
-echo "[.dotfiles/raspberry-pi] Installing packages"
-sudo apt --assume-yes install vim zsh curl wget git neovim tmux
+echo "[.dotfiles/raspberry-pi] Installing apt packages"
+sudo apt --assume-yes install vim zsh curl wget git tmux snapd
+
+echo "[.dotfiles/raspberry-pi] Installing snap packages"
+sudo snap install core
+sudo snap install nvim --classic
 
 echo "[.dotfiles/raspberry-pi] Start zsh configuration"
 source "$DOTFILES"/zsh/install.sh
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -2,6 +2,9 @@
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 export PATH="/usr/local/opt/python@3.10/bin:$PATH"
 
+# Add path for snap when in raspberry-pi
+export PATH="/snap/bin:$PATH"
+
 # Path to your oh-my-zsh installation.
 export ZSH="$HOME/.oh-my-zsh"
 
