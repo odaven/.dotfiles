f86727c6d5db82c1ab34cee87e08e994a65a28fc
odaven <18171281+odaven@users.noreply.github.com>
2022-07-22 23:53:51 +0100

    Added nvim-treesitter and basic configuration

---
+++ neovim/nvim/lua/user/plugins-config/nvim-treesitter.lua
@@ -0,0 +1,11 @@
+require'nvim-treesitter.configs'.setup {
+
+    ensure_installed = {"lua", "python"},
+    
+    highlight = {
+        enable = true,
+    },
+    indent = {
+        enable = false,
+    }
+}
