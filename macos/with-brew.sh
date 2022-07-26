e0bab20e4f6b637b30d9b13cf80397bf062604d7
odaven <18171281+odaven@users.noreply.github.com>
2022-07-26 18:13:23 +0100

    Adding verbose flag

--- macos/with-brew.sh
+++ macos/with-brew.sh
@@ -1,7 +1,7 @@
 DOTFILES="$HOME/.dotfiles"
 
 echo "[.dotfiles/macos/with-brew] Installing software with homebrew"
-brew bundle install --file "$DOTFILES"/macos/Brewfile
+brew bundle install --verbose --file "$DOTFILES"/macos/Brewfile
 
 echo "[.dotfiles/macos/with-brew] Pointing at the new openjdk"
 sudo ln -sfn /usr/local/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
