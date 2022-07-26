92fa2f988fad426a865cb81b4d32022484d02921
odaven <18171281+odaven@users.noreply.github.com>
2022-07-26 22:28:21 +0100

    Avoid the errors from not finding files in after/plugin during the install as they are noise only

--- neovim/install.sh
+++ neovim/install.sh
@@ -5,8 +5,9 @@ mkdir -p ~/.config
 ln -sfv "$DOTFILES"/neovim/nvim ~/.config
 
 echo "[.dotfiles/neovim] Install packer.nvim"
+rm -rf ~/.local/share/nvim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 echo "[.dotfiles/neovim] Installing plugins"
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync' > /dev/null 2>&1
