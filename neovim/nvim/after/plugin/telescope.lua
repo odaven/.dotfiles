6efba66a7485d0f4f898d009889a24fef51e4c13
odaven <18171281+odaven@users.noreply.github.com>
2022-08-26 23:08:30 +0100

    Show all the pickers with C-p and add the <leader>fg for git_files

--- neovim/nvim/after/plugin/telescope.lua
+++ neovim/nvim/after/plugin/telescope.lua
@@ -2,10 +2,19 @@ local opts = { noremap = true, silent = true }
 
 local keymap = vim.api.nvim_set_keymap
 
+keymap("n", "<C-p>", ":Telescope<CR>", opts)
+
 keymap("n", "<leader>ff", ":Telescope find_files<CR>", opts)
-keymap("n", "<leader>fg", ":Telescope live_grep<CR>", opts)
+keymap("n", "<leader>fg", ":Telescope git_files<CR>", opts)
+keymap("n", "<leader>fl", ":Telescope live_grep<CR>", opts)
 keymap("n", "<leader>fb", ":Telescope buffers<CR>", opts)
 keymap("n", "<leader>fh", ":Telescope help_tags<CR>", opts)
 
 
-require('telescope').setup()
+require('telescope').setup{
+    defaults = {
+        file_ignore_patterns = {
+            -- "node_modules"
+        }
+    }
+}
