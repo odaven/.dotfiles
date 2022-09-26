1ec53b813a068bbec04b0a2f77cacb5315f02ce8
odaven <18171281+odaven@users.noreply.github.com>
2022-09-26 22:44:32 +0100

    Highlight the yanked text

---
+++ neovim/nvim/lua/user/behaviours.lua
@@ -0,0 +1,12 @@
+-- Highlight the yanked text
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+
+    group = highlight_group,
+    pattern = '*',
+})
+
