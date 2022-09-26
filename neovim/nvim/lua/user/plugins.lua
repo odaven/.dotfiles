a5f623848f42374223d85f5e03fed4afbd295886
odaven <18171281+odaven@users.noreply.github.com>
2022-09-26 21:59:57 +0100

    Adding gitsigns

--- neovim/nvim/lua/user/plugins.lua
+++ neovim/nvim/lua/user/plugins.lua
@@ -30,6 +30,8 @@ return require('packer').startup(function(use)
 
     use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 
+    use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } }
+
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if packer_bootstrap then
