a253fc0b39403d6fde857fc95b42893f3634eb55
odaven <18171281+odaven@users.noreply.github.com>
2023-01-05 23:48:29 +0000

    Adding javascript to treesitter

--- neovim/nvim/after/plugin/nvim-treesitter.lua
+++ neovim/nvim/after/plugin/nvim-treesitter.lua
@@ -1,6 +1,6 @@
 require('nvim-treesitter.configs').setup {
 
-    ensure_installed = {"lua", "typescript"},
+    ensure_installed = {"javascript", "lua", "typescript"},
 
     highlight = {
         enable = true,
