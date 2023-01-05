e9ebad2c9b0af530bbc76ecf2139e3a97a84eed8
odaven <18171281+odaven@users.noreply.github.com>
2023-01-05 23:28:56 +0000

    Change languages for treesitter

--- neovim/nvim/after/plugin/nvim-treesitter.lua
+++ neovim/nvim/after/plugin/nvim-treesitter.lua
@@ -1,12 +1,12 @@
-require'nvim-treesitter.configs'.setup {
+require('nvim-treesitter.configs').setup {
 
-    ensure_installed = {"lua", "python"},
-    
+    ensure_installed = {"lua", "typescript"},
+
     highlight = {
         enable = true,
+        additional_vim_regex_highlighting = false
     },
     indent = {
         enable = true,
     }
 }
-
