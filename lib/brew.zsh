f905fc39c237743b0694339e5528e6f4e6ea0018
odaven <18171281+odaven@users.noreply.github.com>
2024-03-12 18:13:35 +0000

    Don't check if brew exists before trying to install it. Just do it.

--- lib/brew.zsh
+++ lib/brew.zsh
@@ -1,10 +1,5 @@
 #!/usr/bin/env zsh
 
 function install_brew() {
-  if test ! "$(which brew)"; then
-    echo "Installing Homebrew"
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  else
-    echo "Homebrew was already installed"
-  fi
+  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 }
