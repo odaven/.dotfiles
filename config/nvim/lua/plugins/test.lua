5c56709f1aa2f596af380508d5be424496003134
odaven <18171281+odaven@users.noreply.github.com>
2024-07-24 22:31:19 +0100

    lazyvim: Adding neotest-jest

--- config/nvim/lua/plugins/test.lua
+++ config/nvim/lua/plugins/test.lua
@@ -2,9 +2,13 @@ return {
   {
     "nvim-neotest/neotest",
     optional = true,
-    dependencies = { "marilari88/neotest-vitest" },
+    dependencies = {
+      "marilari88/neotest-vitest",
+      "nvim-neotest/neotest-jest",
+    },
     opts = {
       adapters = {
+        ["neotest-jest"] = {},
         ["neotest-vitest"] = {},
       },
     },
