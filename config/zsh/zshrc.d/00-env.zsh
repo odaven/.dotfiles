f148c602b6c7a725a4329b8e3f8cfb69a89a0aeb
odaven <18171281+odaven@users.noreply.github.com>
2025-06-20 17:49:23 +0100

    Setting tokyonight for most things

--- config/zsh/zshrc.d/00-env.zsh
+++ config/zsh/zshrc.d/00-env.zsh
@@ -32,6 +32,31 @@ export FZF_CTRL_R_OPTS=$'
   --preview-window down:3:hidden:wrap
   --bind \'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort\''
 
+# Tokyonight night
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+  --highlight-line \
+  --info=inline-right \
+  --ansi \
+  --layout=reverse \
+  --border=none \
+  --color=bg+:#283457 \
+  --color=bg:#16161e \
+  --color=border:#27a1b9 \
+  --color=fg:#c0caf5 \
+  --color=gutter:#16161e \
+  --color=header:#ff9e64 \
+  --color=hl+:#2ac3de \
+  --color=hl:#2ac3de \
+  --color=info:#545c7e \
+  --color=marker:#ff007c \
+  --color=pointer:#ff007c \
+  --color=prompt:#2ac3de \
+  --color=query:#c0caf5:regular \
+  --color=scrollbar:#27a1b9 \
+  --color=separator:#ff9e64 \
+  --color=spinner:#ff007c \
+"
+
 # Go
 export GOPATH="$XDG_DATA_HOME/go"
 export GOBIN="$GOPATH/bin"
