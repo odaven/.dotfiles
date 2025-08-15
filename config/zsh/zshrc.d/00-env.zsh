12abd56eca0673bfcc2a9337b84a3f08e81f7731
odaven <18171281+odaven@users.noreply.github.com>
2025-08-15 22:25:06 +0100

    Envs for less and python3

--- config/zsh/zshrc.d/00-env.zsh
+++ config/zsh/zshrc.d/00-env.zsh
@@ -38,6 +38,10 @@ export GOBIN="$GOPATH/bin"
 export GOCACHE="$XDG_CACHE_HOME/go-build"
 export GOMODCACHE="$XDG_CACHE_HOME/go/mod"
 
+# Less
+[ -d "$XDG_STATE_HOME/less" ] || mkdir -p "$XDG_STATE_HOME/less"
+export LESSHISTFILE="$XDG_STATE_HOME/less/history"
+
 # Neovim as manpager
 export MANPAGER="nvim +Man!"
 
@@ -46,6 +50,15 @@ export NODE_REPL_HISTORY="$XDG_DATA_HOME/node_repl_history"
 
 # Npm
 export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/npmrc"
+
+# Python
+export PYTHONPYCACHEPREFIX=$XDG_CACHE_HOME/python
+export PYTHONUSERBASE=$XDG_DATA_HOME/python
+PYTHON_STATE_HOME="$XDG_STATE_HOME/python"
+export PYTHON_HISTORY="$PYTHON_STATE_HOME/history"
+[ -d "$PYTHONPYCACHEPREFIX" ] || mkdir -p "$PYTHONPYCACHEPREFIX"
+[ -d "$PYTHONUSERBASE" ] || mkdir -p "$PYTHONUSERBASE"
+[ -d "$PYTHON_STATE_HOME" ] || mkdir -p "$PYTHON_STATE_HOME"
 
 # Ripgrep
 export RIPGREP_CONFIG_PATH="$XDG_CONFIG_HOME/ripgrep/config"
