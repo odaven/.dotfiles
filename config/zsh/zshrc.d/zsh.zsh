4e5c4a73af3f61fe89eb0180f3eb4ea335819d60
odaven <18171281+odaven@users.noreply.github.com>
2026-03-19 09:40:32 +0000

    Change colours for the terminal

--- config/zsh/zshrc.d/zsh.zsh
+++ config/zsh/zshrc.d/zsh.zsh
@@ -16,7 +16,9 @@ zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:
 zstyle ':completion:*' group-name ''
 zstyle ':completion:*:descriptions' format '%F{yellow}-- %d --%f'
 
-# Colors in completion menu (uses LS_COLORS)
+# Directory colors (consistent across macOS ls, GNU ls, and completion menu)
+export LSCOLORS="Gxfxcxdxbxegedabagacad"
+export LS_COLORS="${LS_COLORS:+$LS_COLORS:}di=01;36:ln=35"
 zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
 
 # Complete . and .. directories
