2a6d621a9b6dce7dc9c7fc6da1d27de7fb3888f6
odaven <18171281+odaven@users.noreply.github.com>
2026-05-21 14:56:02 +0100

    feat(nvim): replace submodule with clone-on-demand in dot()

--- modules/nvim.zsh
+++ modules/nvim.zsh
@@ -1,6 +1,12 @@
 source "$HOME/.dotfiles/lib/functions.zsh"
 
 config() {
+  local repo="https://github.com/odaven/nvim.git"
+  local src="$HOME/.dotfiles/config/nvim"
+
+  rm -rf "$src"
+  git clone "$repo" "$src"
+
   dot
 }
 
@@ -11,11 +17,11 @@ clean() {
 dev() {
   local src="$HOME/Dev/nvim"
   local dest="$(config_dir)/nvim"
+
   [[ -e $dest || -L $dest ]] && rm -rf "$dest"
   ln -sv "$src" "$dest"
 }
 
 dot() {
-  git -C "$HOME/.dotfiles" submodule update --init --recursive --remote --force
   "$HOME/.dotfiles/bin/link" "config/nvim" "$(config_dir)/nvim"
 }
